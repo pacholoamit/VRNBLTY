3efa2f288896879bfab5cbad64fe8c3ba078128c
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-19 03:17:21 +0800

    formatted number in BreachDetails and added additional information in modal

--- src/popup/BreachDetails.tsx
+++ src/popup/BreachDetails.tsx
@@ -1,9 +1,13 @@
-import { Stack, Text, Button, Collapse, List } from "@mantine/core"
+import { Stack, Text, Button, Collapse, List, Center, Title } from "@mantine/core"
 import React from "react"
-import { Database } from "tabler-icons-react"
+import { Ce, Database } from "tabler-icons-react"
 import type { Breach, styles } from "../types"
 
 const styles: styles = {
+    img: {
+        backgroundColor: 'snow',
+        marginRight: 10
+    },
     text: {
         color: "snow"
     }
@@ -18,11 +22,15 @@ const BreachDetails: React.FC<BreachDetailsProps> = ({ breach }) => {
     const onClick = () => setOpened(!opened)
     return (
         <>
+            <Center>
+                <img src={breach.LogoPath} alt={breach.Name} height='30' width={'30'} style={styles.img} />
+                <Title style={styles.text} order={5}>{breach.Name} Breaches</Title>
+            </Center>
             <Button leftIcon={<Database />} color={'red'} variant={'light'} onClick={onClick}>Date: {breach.BreachDate} </Button>
             <Collapse in={opened}>
                 <List style={styles.text}>
                     <List.Item>Domain: {breach.Domain}</List.Item>
-                    <List.Item>Affected: {breach.PwnCount} users</List.Item>
+                    <List.Item>Users: {breach.PwnCount.toLocaleString()}</List.Item>
                     <List.Item>Data Breached: </List.Item>
                     <List style={styles.text} withPadding >
                         {breach?.DataClasses?.map((dataClass) => (
